08e1bee8a70ced507724643fc9013016d2a22936
Alius999 <myzikant999@gmail.com>
2026-02-01 16:01:01 +0500

    fixing path

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -12,7 +12,7 @@ import { CommentsModule } from './comments/comments.module';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'),
+      rootPath: join(process.cwd(), 'uploads'),
       serveRoot: '/uploads',
     }),
     RegistrationModule, 
